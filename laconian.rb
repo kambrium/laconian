48aaca138216e9cd02485ce82f7543144a0d9f34
kambrium <kambrium@users.noreply.github.com>
2023-05-07 19:39:10 +0200

    File and directory handling

--- laconian.rb
+++ laconian.rb
@@ -2,6 +2,20 @@ require 'cgi'
 require 'date'
 require 'socket'
 
+root = "."
+
+# def strip_char(string, chars)
+#   chars = Regexp.escape(chars)
+#   string.gsub(/\A[#{chars}]+|[#{chars}]+\z/, "")
+# end
+
+def write_file(client, file_path)
+  client.puts("2 text/gemini\r\n")
+  file_stream = File.new(file_path, 'rb')
+  IO::copy_stream(file_stream, client)
+  file_stream.close
+end
+
 server = TCPServer.new 'localhost', 1965
 
 loop do
@@ -12,12 +26,23 @@ loop do
     hostname, path, content_length = request.split(" ")
 
     path = CGI.unescape(path)
-    path.slice!(0)
+    # Yes, this is bad! Possible substitution see above.
+    cpath = path.chomp("/").reverse.chomp("/").reverse
+    file_path = "#{root}/#{cpath}"
 
-    client.puts("2 text/gemini\r\n")
-    file_stream = File.new(path, 'rb')
-    IO::copy_stream(file_stream, client)
-    file_stream.close
+    if File.file?(file_path)
+      write_file(client, file_path)
+    elsif File.directory?(file_path)
+      if !path.end_with?("/")
+        client.puts("3 #{path}/\r\n")
+      elsif File.file?("#{file_path}/index.gmi")
+        write_file(client, "#{file_path}/index.gmi")
+      else
+        client.puts("2 text/gemini\r\n")
+        client.puts("=>..\n")
+        Dir.each_child(file_path) {|x| puts "Got #{x}" }
+      end
+    end
 
     client.close
   end
